6f0efdc74ccb0b1f0583a4ed5f8575c690f70889
Nikhil <nikhil.sonnad@gmail.com>
2013-11-27 14:34:41 +1100

    caught a typo in text

--- transform/parse_html.py
+++ transform/parse_html.py
@@ -8,10 +8,11 @@ def cull_html(input_file):
     cull = []
     soup = BeautifulSoup(open(input_file))
 
-    pattern = re.compile(r'c\d+')
+    # get all of the ps and h2s in the document
     p_h2s = soup.find_all(['h2', 'p'])
 
     for p_h2 in p_h2s:
+        # ignore the footnotes
         css_class = p_h2.get('class')
         if css_class and 'sdfootnote' not in css_class:
             cull.append(re.sub(r'(\/)\s', '\1', p_h2.text.replace('\n', '')))
@@ -21,6 +22,7 @@ def cull_html(input_file):
 
 def parse_html(html_in):
     parsed = '\n'.join(cull_html(html_in))
+    # this pattern appears to catch all the decisions, as well as the subsequent word (if there is one)
     pro_opp_pattern = re.compile(r'((?:(?:CA|US|VN|PE|BN|NZ|AU|MX|SG|MY|CL|JP)\/*)+\]*\s*)(\w+)?', re.UNICODE)
     matches = pro_opp_pattern.finditer(parsed)
     d = []

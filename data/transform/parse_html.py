e124819a402cc9724367d582bb49378979cdf0e0
Nikhil <nikhil.sonnad@gmail.com>
2013-12-03 17:53:07 +1100

    regex for htmlifying this text is getting fugly

--- data/transform/parse_html.py
+++ data/transform/parse_html.py
@@ -10,6 +10,7 @@ def cull_html(input_file):
 
     # get all of the ps and h2s in the document
     p_h2s = soup.find_all(['h2', 'p'])
+    output_clean(p_h2s)
 
     for p_h2 in p_h2s:
         # ignore the footnotes
@@ -19,10 +20,58 @@ def cull_html(input_file):
 
     return cull
 
-    
-def parse_html(html_in):
+def output_clean(tags_in):
     somehtml = ''
     outhtml = open('parsed.html', 'wb')
+
+    pro_opp_pattern = re.compile(
+        r'''
+        (\[?
+        (?:(?:CA|US|VN|PE|BN|NZ|AU|MX|SG|MY|CL|JP)
+        (?:\/*\]?\s*))+
+        )
+        (
+        .+?
+        (?=\[|\]|CA|US|VN|PE|BN|NZ|AU|MX|SG|MY|CL|JP)
+        \]?
+        )
+        ''',
+        re.UNICODE | re.VERBOSE | re.DOTALL | re.MULTILINE)
+
+    firstp = True
+
+    for tag in tags_in:
+        if tag.name != 'h2':
+            row = tag.text
+            m = pro_opp_pattern.findall(row)
+            if m:
+                somehtml += '</p>'
+                rowhtml = ''
+                firstp = True
+                for match in m:
+                    groups = match
+                    countries = groups[0].replace(' ', '').replace('/', ' ')
+                    print groups[1]
+                    text = groups[1]
+                    spantag = ' <span class=countries "' + countries + '">' + groups[0] + '</span>' + text
+                    rowhtml += spantag
+
+                somehtml += ''.join(['<p>', rowhtml, '</p>'])
+            else:
+                if len(row) > 1:
+                    if firstp:
+                        somehtml += '<p>' + row
+                        firstp = False
+                    else:
+                        somehtml += row
+
+        else:
+            somehtml += '<h2>' + tag.text + '</h2>' + '\n'
+
+    outhtml.write(somehtml.encode('utf8'))
+    outhtml.close()
+    
+def parse_html(html_in):
     parsed = '\n'.join(cull_html(html_in))
     # this pattern appears to catch all the decisions, as well as the subsequent word (if there is one)
 
@@ -36,33 +85,13 @@ def parse_html(html_in):
         (\w+)?
         (
         .+?
-        (?:\[|\]|CA|US|VN|PE|BN|NZ|AU|MX|SG|MY|CL|JP)
+        (?:\[|\]|CA|US|VN|PE|BN|NZ|AU|MX|SG|MY|CL|JP)?
         )
         ''',
         re.UNICODE | re.VERBOSE)
 
     matches = pro_opp_pattern.finditer(parsed)
     d = []
-
-    for row in parsed.split('\n'):
-        m = pro_opp_pattern.findall(row)
-        rowhtml = ''
-        if m:
-            for match in m:
-                groups = match
-                countries = groups[0].replace(' ', '').replace('/', ' ')
-                stance = groups[1].lower() if groups[1] else ''
-                text = stance + groups[2]
-                spantag = '<span class="' + countries + '">' + groups[0] + '</span>' + text
-                rowhtml += spantag
-
-            somehtml += ''.join(['<p>', rowhtml, '</p>'])
-        else:
-            if len(row) > 1:
-                somehtml += '<p>' + row + '</p>' + '\n'
-
-    outhtml.write(somehtml.encode('utf8'))
-    outhtml.close()
 
     for m in matches:
         groups = m.groups()

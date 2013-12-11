2eafae5ecab9b1de00af5c594861576324112d32
B.J. Rossiter <b.j.rossiter@gmail.com>
2013-12-11 11:24:02 +1100

    voting_similarity.csv is now generated by data/snippet.js

--- data/snippets.js
+++ data/snippets.js
@@ -4,6 +4,22 @@ var _ = require('lodash');
 var cheerio = require('cheerio');
 var Combinatorics = require('js-combinatorics').Combinatorics;
 
+var countries = 'Australia,Brunei,Canada,Chile,Japan,Mexico,Malaysia,New Zealand,Peru,Singapore,United States,Vietnam'.split(',');
+var abbrevs = {
+  'Australia': 'AU',
+  'Brunei': 'BN',
+  'Canada': 'CA',
+  'Chile': 'CL',
+  'Japan': 'JP',
+  'Mexico': 'MX',
+  'Malaysia': 'MY',
+  'New Zealand': 'NZ',
+  'Peru': 'PE',
+  'Singapore': 'SG',
+  'United States': 'US',
+  'Vietnam': 'VN'
+};
+
 // pull out newlines
 var strippedHtml = wikiHtml.replace(/(\r\n|\n|\r)/gm, ' ');
 // strip out all spans (some are malformed)
@@ -25,6 +41,20 @@ $('h1').removeClass().addClass('tpp-big-head');
 $('h2').removeClass().addClass('tpp-med-head');
 
 var highlightIndex = 0;
+var allCombos = {};
+var allCountries = {};
+
+function recordCombo(combo){
+  var key = combo.sort().join('');
+  allCombos[key] = allCombos[key] ? allCombos[key] + 1 : 1;
+}
+
+function recordCountry(country){
+  allCountries[country] = allCountries[country] ? allCountries[country] + 1 : 1;
+}
+// record each combo
+// record each vote by country
+// generate 2d array of countries
 
 // split on paragraphs
 $('p').each(function(){
@@ -37,7 +67,9 @@ $('p').each(function(){
     var parsedCountries = countriesMatch.match(/([\w\/]+)(.*)$/);
     var countryList = parsedCountries[1].split('/');
     var bitAtEnd = parsedCountries[2];
-    if (countries.length < 2) return countriesMatch;
+    if (countries.length < 2){
+      return countriesMatch;
+    }
     // Drop footnotes where it is not a proposal
     if (html.match(/^\d\d+/) && !html.match(/oppose|propose/)){
       //console.error('dropping:');
@@ -45,9 +77,13 @@ $('p').each(function(){
       return countriesMatch;
     }
     var combos = Combinatorics.combination(countries, 2).toArray();
+    // Records country and combos for later
+    _.each(countries, recordCountry);
+    _.each(combos, recordCombo);
     var dataAttrs = _.map(combos, function(combo){
-      var dataAttr = 'data-' + combo.join('') + '="true"';
-      $p.attr('data-' + combo.join(''), 'true');
+      var key = combo.sort().join('');
+      var dataAttr = 'data-' + key + '="true"';
+      $p.attr('data-' + key, 'true');
       return dataAttr;
     });
     var replacedHtml = '<span class="covotes" ' +
@@ -58,8 +94,18 @@ $('p').each(function(){
     highlightIndex++;
     return replacedHtml;
   });
+
+  // scan again for single countries
+  replacedHtml = replacedHtml.replace(/(p>|\[| )(CA|US|VN|PE|BN|NZ|AU|MX|SG|MY|CL|JP)( oppose|propose)/g, function(match, before, country, after){
+    recordCountry(country);
+    //console.log('found ' + country + ' in  ' + match);
+    //console.log(string);
+    return before + '<strong data-country="'+country+'">' + country + '</strong>' + after;
+  });
+
   $p.html(replacedHtml + '\n\n');
 });
+
 
 // put it back into the index doc
 var indexPath = __dirname + '/../app/index.html';
@@ -67,3 +113,22 @@ var indexHtml = fs.readFileSync(indexPath, {encoding: 'utf8'});
 var $index = cheerio.load(indexHtml);
 $index('#transcripts').html($.html());
 fs.writeFileSync(indexPath, $index.html(), {encoding: 'utf8'});
+
+// write out csv doc
+var csvPath = __dirname + '/../app/data/voting_similarity.csv';
+var lines = ['voting_country,partner,sim_votes,sim_pct,baseline'];
+_.each(countries, function(voter){
+  var voterAbbrev = abbrevs[voter];
+  var voterTotal = allCountries[voterAbbrev];
+  _.each(countries, function(partner){
+    var covote, partnerAbbrev;
+    if (partner === voter){
+      covote = voterTotal;
+    }else{
+      partnerAbbrev  = abbrevs[partner];
+      covote = allCombos[[voterAbbrev,partnerAbbrev].sort().join('')];
+    }
+    lines.push([voter, partner, covote, covote/voterTotal, voterTotal].join(','));
+  });
+});
+fs.writeFileSync(csvPath, lines.join('\n'), {encoding: 'utf8'});
